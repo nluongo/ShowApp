0ed0ef5b0ec6a4f3b12dafaa20eb855483237fca
nluongo <nluongo@gmail.com>
2018-10-19 21:44:47 -0700

    Change GetIMDBTopShows to Linux file paths

--- Scripts/GetIMDBTopShows.py
+++ Scripts/GetIMDBTopShows.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import re
 import sqlite3
 
-conn = sqlite3.connect("C:\ShowApp\Databases\TopShows.db")
+conn = sqlite3.connect("../Databases/TopShows.db")
 c = conn.cursor()
 
 imdburl = 'http://www.imdb.com/search/title?title_type=tv_series'
 html = urllib.request.urlopen(imdburl).read()
 soup = BeautifulSoup(html, "html.parser")
 asoup = soup.find_all("a",href=re.compile("/title/tt"+".+"+"adv_li_tt"),text=re.compile("\w"))
-writefile = open(r'C:\Users\Nicholas\Desktop\Soup.txt', "w")
+writefile = open(r'../Databases/Soup.txt', "w")
 for i in asoup:
     c.execute("INSERT INTO TopShows ( ShowName ) VALUES ( ? )", (i.text,))
     writefile.write(i.text+"\n")
